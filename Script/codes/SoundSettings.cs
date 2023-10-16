1df76c83d4961b220075ef675acac3653eff7286
Vernwalker <106093316+20145050-Vernon-Ong@users.noreply.github.com>
2023-10-16 11:33:56 +0800

    Add files via upload

--- Script/codes/SoundSettings.cs
+++ Script/codes/SoundSettings.cs
@@ -6,19 +6,41 @@ public class SoundSettings : MonoBehaviour
     {
     [SerializeField] Slider soundSlider;
     [SerializeField] AudioMixer masterMixer;
-
+    public GameObject soundMute;
+    public GameObject soundunMute;
     // Start is called before the first frame update
     private void Start()
     {
+        if (soundMute == true && PlayerPrefs.GetFloat("SavedMasterVolume", 100) > .001f)
+        {
+            soundMute.SetActive(false);
+            soundunMute.SetActive(true);
+            Debug.Log("unmute");
+        }
+        if(PlayerPrefs.GetFloat("SavedMasterVolume", 100) == .001f)
+        {
+            soundMute.SetActive(true);
+            soundunMute.SetActive(false);
+            Debug.Log("mute");
+        }
         SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
     }
 
     public void SetVolume(float _value)
     {
-        if(_value < 1)
+        if(Mathf.Approximately(_value , 0))
         {
             _value = .001f;
-        } 
+            soundMute.SetActive(true);
+            soundunMute.SetActive(false);
+            Debug.Log("mute");
+        }
+        if(soundMute == true && _value > .001f)
+        {
+            soundMute.SetActive(false);
+            soundunMute.SetActive(true);
+            Debug.Log("unmute");
+        }
 
         RefreshSlider(_value);
         PlayerPrefs.SetFloat("SavedMasterVolume", _value);
