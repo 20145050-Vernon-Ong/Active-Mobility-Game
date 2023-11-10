efdc8d4fce30720750852a3b6e66f56abc9df346
Vernwalker <106093316+20145050-Vernon-Ong@users.noreply.github.com>
2023-11-10 10:35:36 +0800

    Add files via upload

--- Script/codes/MapLoaderScript.cs
+++ Script/codes/MapLoaderScript.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using SystemInfo = UnityEngine.Device.SystemInfo;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
@@ -9,16 +8,7 @@ public class MapLoaderScript : MonoBehaviour
     public GameObject loadingScreen;
     public Slider loadingBar;
     public TMP_Text progressText;
-    void Start()
-    {
-        if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.Other)
-        {
-            PlayerPrefs.SetInt("mobile", 1); 
-        } else
-        {
-            PlayerPrefs.SetInt("mobile", 0);
-        }
-    }
+
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
